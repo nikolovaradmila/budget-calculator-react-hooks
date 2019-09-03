bb6a111ab68052334ae156dad897d7cd2935c382
nikolovaradmila <nikolovaradmila@gmail.com>
2019-09-03 14:27:38 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import ExpenseList from "./components/ExpenseList"
 import ExpenseForm from "./components/ExpenseForm"
 import Alert from "./components/Alert"
 import uuid from "uuid/v4"
 
-function App() {
+
+// useState is a function that returns an array with two values
+// the first value is the actual value of the stat
+// the second value is the fuction that we use to update/control the state
+// we can add a default value of the state
+
+
+const initialExpenses = [
+  {
+    id: uuid(), 
+    charge: "rent",
+    amount: 900,
+  },
+  {
+    id: uuid(), 
+    charge: "car payment",
+    amount: 300,
+  },
+  {
+    id: uuid(), 
+    charge: "credit card bill",
+    amount: 200,
+  },
+];
+
+console.log(initialExpenses);
+
+
+function App(){
+  const [expenses, setExpenses] = useState(initialExpenses); 
+  
   return (
     <>
   <Alert/>
+  <h1>Budget Calculator</h1>
+  <main className="main-container">
   <ExpenseForm/>
-  <ExpenseList></ExpenseList>
+  <ExpenseList expenses={expenses}></ExpenseList>
+  </main>
+
+  <h1>
+    total spendings: <span className="total">
+     ${expenses.reduce((acc, curr) => {
+       return (acc += curr.amount);
+     }, 0)}
+    </span>
+  </h1>
     </>
   );
 }
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import ExpenseItem from "./ExpenseItem"
+import { MdDelete } from "react-icons/md";
 
-const ExpenseList = () => {
+const ExpenseList = ({expenses}) => {
+    console.log(expenses)
     return (
-        <div>
-            Hello from expense list
-            <ExpenseItem/>
-        </div>
+        <>
+             <ul className="list">
+                {expenses.map((expense) => {
+                   return <ExpenseItem key={expense.id} expense={expense}></ExpenseItem>
+                })}
+             </ul>
+             {expenses.length > 0 && <button className="btn">clear expenses <MdDelete className="btn-icon"/></button>}
+        </>
     )
 }
 
